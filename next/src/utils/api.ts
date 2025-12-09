531d2ae5fc3f7754b152ba1ee3ee151853208b9a
Rustam <punk60826@gmail.com>
2025-12-09 23:09:05 +0200

    next

--- next/src/utils/api.ts
+++ next/src/utils/api.ts
@@ -1,5 +1,5 @@
 import { DOMAIN_BACKEND } from '@/_data/constants';
-import { Movie } from '@/interfaces';
+import { GetMoviesParams, Movie } from '@/interfaces';
 export type MoviesResponse = {
     data: Movie[];
     movies?: Movie[];
@@ -18,6 +18,27 @@ export async function getMovies({
     const params = new URLSearchParams();
     params.set('page', String(page));
     params.set('limit', String(limit));
+
+    if(category){
+    params.set('category', category);
+
+    }
+
+      if (year) {
+    params.set('year', String(year));
+  }
+  
+  if (minRating) {
+    params.set('minRating', String(minRating));
+  }
+  
+  if (orderBy) {
+    params.set('orderBy', orderBy);
+  }
+  
+  if (order) {
+    params.set('order', order);
+  }
     const res = await fetch(`${DOMAIN_BACKEND}/movie?${params.toString()}`, {
         cache: 'no-store',
     });
