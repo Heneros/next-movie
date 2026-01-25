93c8175b7bd59631519197189eb31cdff8facec5
Heneros <punk60826@gmail.com>
2026-01-25 18:30:13 +0200

    prices lang and adaptive

--- next/src/components/homepage/suggestions/Suggestions.tsx
+++ next/src/components/homepage/suggestions/Suggestions.tsx
@@ -7,6 +7,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBagShopping, faShoppingBag } from '@fortawesome/free-solid-svg-icons'
 import { useAppSelector } from '@/redux/hooks'
 import { RootState } from '@/redux/store';
+import { useTranslations } from 'next-intl'
 
 
 
@@ -16,14 +17,20 @@ function Card({
     period,
     price,
     oldPrice,
+    btnText,
+    cancelAny,
     featured = false,
+
 }: {
     image: any
     title: string
     period: string
     price: string
     oldPrice?: string
+    btnText?: string
+    cancelAny?: string
     featured?: boolean
+
 }) {
 
 
@@ -34,28 +41,32 @@ function Card({
         <Image
             src={image}
             alt={title}
-          //  width={featured ? 300 : 260}
-        className="w-full md:w-auto"
+            //  width={featured ? 300 : 260}
+            className="w-100 sm:w-full "
 
-        />      <div className={`absolute inset-0 flex flex-col items-center justify-between py-8 text-center pointer-events-auto ${featured ? "top-1/5" : "top-15"}`}>
+        />      <div className={`absolute inset-0 flex flex-col items-center justify-between py-8 text-center pointer-events-auto ${featured ? "top-1/5" : "lg:top-15 top-5"}`}>
             <div>
                 <h3 className={`text-4xl font-lato tracking-wide ${featured ? 'text-white' : 'text-primary-second'}`}> {title}  </h3>
 
-                <p className={`text-2xl opacity-80 mt-5 ${featured ? 'text-white' : 'text-primary-second'}`}>{period}</p>
+                <p className={`text-2xl opacity-80 lg:mt-5   ${featured ? 'text-white' : 'text-primary-second'}`}>{period}</p>
             </div>
 
-            <div className={`flex absolute items-center flex-col  ${featured ? "top-2/5 gap-3" : " gap-5 top-4/7"}`}>
+            {/* Bottom */}
+            <div className={`flex flex-col items-center w-full max-w-[90%] ${featured ? "gap-3  mb-45 md:mb-30 lg:mb-50" : "gap-5  mb-25 md:mb-5 lg:mb-25"}`}>
+
                 {oldPrice && (
                     <p className={`text-2xl line-through opacity-60`}>{oldPrice}</p>
                 )}
-                <p className={` font-bold ${featured ? 'text-3xl' : 'text-sky-500 text-4xl '}`}>
+                <p className={` font-bold ${featured ? 'text-4xl' : 'text-sky-500 text-xl lg:text-4xl '}`}>
                     {price}
                 </p>
-                <p className={`text-sm opacity-60 mt-1 ${featured ? '' : ' text-[#fff]  dark:text-[#000]'}`}>Cancel anytime</p>
-                <button className={`min-w-30 py-2 px-3 flex items-baseline justify-center gap-2  h-auto rounded-2xl ${featured ? "bg-light-main text-primary-second" : "bg-primary-second text-light-main "} `}>
+                <p className={`text-sm opacity-60 mt-1 ${featured ? '' : ' text-[#fff]  dark:text-[#000]'}`}>{cancelAny} </p>
+
+
+                <button className={`w-50 py-2 px-3 flex items-baseline justify-center gap-2  h-[35%] rounded-2xl ${featured ? "bg-light-main text-primary-second" : "bg-primary-second text-light-main "} `}>
                     <FontAwesomeIcon icon={faBagShopping} />
 
-                    <span className={`font-lato text-base not-italic  cursor-pointer uppercase tracking-wide `}>Continue</span>
+                    <span className={`font-lato text-base not-italic  cursor-pointer uppercase tracking-wide `}>{btnText}</span>
                 </button>
             </div>
 
@@ -66,6 +77,7 @@ function Card({
 
 
 export default function Suggestions() {
+    const t = useTranslations("Prices");
     const theme = useAppSelector((s: RootState) => s.theme.mode);
     const imageBasic = theme === 'dark' ? BasicSuggestion : DarkBasicSuggestion;
 
@@ -75,25 +87,32 @@ export default function Suggestions() {
 
                 <Card
                     image={imageBasic}
-                    title="Basic"
-                    period="3 month"
+                    title={`${t('basic')}`}
+                    period={`3 ${t('month')}`}
                     price="$15.140"
+                    btnText={`${t('continue')}`}
+                    cancelAny={`${t('cancelAny')}`}
                 />
 
                 <Card
                     image={BlueSuggestion}
-                    title="Suggested"
-                    period="6 month"
+                    title={`${t('suggested')}`}
+                    period={`6 ${t('month')}`}
+
                     oldPrice="$24.990"
                     price="$22.990"
                     featured
+                    btnText={`${t('continue')}`}
+                    cancelAny={`${t('cancelAny')}`}
                 />
 
                 <Card
                     image={imageBasic}
-                    title="Premium"
-                    period="12 month"
+                    title={`${t('premium')}`}
+                    period={`12 ${t('month')}`}
                     price="$35.199"
+                    btnText={`${t('continue')}`}
+                    cancelAny={`${t('cancelAny')}`}
                 />
 
             </div>
